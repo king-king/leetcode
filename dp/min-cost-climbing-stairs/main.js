d4fa9eedee0d6fa25b6fa996c092ef202e6d56be
wangqun28 <wangqun6@jd.com>
2022-07-30 18:30:35 +0800

    update

--- dp/min-cost-climbing-stairs/main.js
+++ dp/min-cost-climbing-stairs/main.js
@@ -4,22 +4,14 @@
  */
 var minCostClimbingStairs = function (cost) {
     // dp[i]表示到达第i个台阶的最小花费
-    const dp = [0, 0];
-    let sum = 0;
-    const min = (a, b) => (a > b ? b : a);
-    cost.forEach(num => { sum += num; });
-    for (let i = 2; i < cost.length; i++) {
-        dp[i] = sum + 1;
+    // 0 1
+    let last2 = 0, last1 = 0;
+    for (let floor = 2; floor <= cost.length; floor++) {
+        const next = Math.min(last2 + cost[floor - 2], last1 + cost[floor - 1]);
+        last2 = last1;
+        last1 = next;
     }
-    for (let floor = 0; floor < cost.length; floor++) {
-        if (floor + 1 < cost.length) {
-            dp[floor + 1] = min(dp[floor + 1], dp[floor] + cost[floor]);
-        }
-        if (floor + 2 < cost.length) {
-            dp[floor + 2] = min(dp[floor + 2], dp[floor] + cost[floor]);
-        }
-    }
-    return min(dp[dp.length - 1] + cost[cost.length - 1], dp[dp.length - 2] + cost[cost.length - 2]);
+    return last1;
 };
 
 console.log(minCostClimbingStairs([10, 15, 20]));
