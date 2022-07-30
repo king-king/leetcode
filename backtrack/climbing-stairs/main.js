9bebf8afc2249e76d0e73ce858713457dad310a5
wangqun28 <wangqun6@jd.com>
2022-07-30 18:44:20 +0800

    update

--- backtrack/climbing-stairs/main.js
+++ backtrack/climbing-stairs/main.js
@@ -1,18 +1,21 @@
 /**
- * @param {number[]} cost
+ * @param {number} n
  * @return {number}
  */
-var minCostClimbingStairs = function (cost) {
-    // dp[i]表示到达第i个台阶的最小花费
-    // 0 1
-    let last2 = 0, last1 = 0;
-    for (let floor = 2; floor <= cost.length; floor++) {
-        const next = Math.min(last2 + cost[floor - 2], last1 + cost[floor - 1]);
-        last2 = last1;
-        last1 = next;
-    }
-    return last1;
+var climbStairs = function (n) {
+    let result = 0;
+    const up = step => {
+        if (step === n) {
+            result++;
+        } else if (step + 1 <= n) {
+            up(step + 1);
+        } else if (step + 2 <= n) {
+            up(step + 2);
+        }
+    };
+    up(n);
+    return result;
 };
 
-console.log(minCostClimbingStairs([10, 15, 20]));
-console.log(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]));
+console.log(climbStairs(2), climbStairs(2) === 2);
+console.log(climbStairs(3), climbStairs(3) === 3);
--- dp/min-cost-climbing-stairs/main.js
+++ dp/min-cost-climbing-stairs/main.js
@@ -1,7 +1,18 @@
 /**
- * @param {number} n
+ * @param {number[]} cost
  * @return {number}
  */
-var climbStairs = function (n) {
-
+var minCostClimbingStairs = function (cost) {
+    // dp[i]表示到达第i个台阶的最小花费
+    // 0 1
+    let last2 = 0, last1 = 0;
+    for (let floor = 2; floor <= cost.length; floor++) {
+        const next = Math.min(last2 + cost[floor - 2], last1 + cost[floor - 1]);
+        last2 = last1;
+        last1 = next;
+    }
+    return last1;
 };
+
+console.log(minCostClimbingStairs([10, 15, 20]));
+console.log(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]));
